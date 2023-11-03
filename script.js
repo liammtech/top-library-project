b9e13281cfd8c3d6c472ec8d334c144fd2ee090b
liammtech <liammtech@gmail.com>
2023-11-03 17:26:02 +0000

    Add book object constructor

--- script.js
+++ script.js
@@ -16,11 +16,11 @@
 
 let myLibrary = [];
 
+// 2. Constructor for book object
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
-
-function addBookToLibrary
