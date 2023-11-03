9df73d81399ba8aaed39875e50e081715b0069b0
liammtech <liammtech@gmail.com>
2023-11-03 17:15:06 +0000

    Add initial pseudocode to js file

---
+++ script.js
@@ -0,0 +1,21 @@
+/* TOP Library Project
+
+1. Create an array to store the books
+
+2. Constructor for book object, needs:
+    - Title
+    - Author
+    - No. of pages
+    - Read it or not
+
+3. Function to add book to library
+
+4. Function to remove book from library
+
+5. Function to remove all books from library
+
+6. Function to toggle "read" status on or off
+
+7. Function to loop through the array and displays all books on the page
+
+*/
