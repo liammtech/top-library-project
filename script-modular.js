139026e2decbd97f1fb07055c1f2267aa9ce3424
liammtech <liammtech@gmail.com>
2023-11-04 16:43:59 +0000

    Add query selectors to cacheDom method

--- script-modular.js
+++ script-modular.js
@@ -14,7 +14,19 @@
         },
 
         // cacheDom: caches document information
-        cacheDom: function() {},
+        cacheDom: function() {
+
+            // Add all document query selectors
+            this.container = document.querySelector("#main-container");
+            this.bookCards = document.querySelectorAll(".book-card");
+            this.addButton = document.querySelector("#add-book-button");
+            this.modal = document.querySelector("#add-book-modal");
+            this.form = document.querySelector("#add-book-form");
+            this.title = document.getElementById("title");
+            this.author = document.getElementById("author");
+            this.pages = document.getElementById("pages");
+            this.read = document.getElementById("read");
+        },
 
         // bindEvents: event handling
         bindEvents: function() {},
