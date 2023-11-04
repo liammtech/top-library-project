925bb4a32a48e0838f05a4d0d07b309a50cd05b6
liammtech <liammtech@gmail.com>
2023-11-04 16:23:22 +0000

    Add empty initialisation functions

--- script-modular.js
+++ script-modular.js
@@ -3,7 +3,24 @@
 
     // Create app object
     var libraryApp = {
-        myLibrary: [],
+
+        // Add init function
+        init: function() {
+
+            // Initialisation functions
+            this.cacheDom();
+            this.bindEvents();
+            this.render();
+        },
+
+        // cacheDom: caches document information
+        cacheDom: function() {},
+
+        // bindEvents: event handling
+        bindEvents: function() {},
+
+        // render: pushes to page document
+        render: function() {},
     };
 
     // Initialise app
